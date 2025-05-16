4e17f3b4911f3bdd175eb3bb70e8ff526aed30f7
Arthur <ipz233_nad@student.ztu.edu.ua>
2025-05-16 20:36:42 +0300

    Додано task3 з лабораторної 4

---
+++ KPZlab4/task3/src/LightElementNode.js
@@ -0,0 +1,36 @@
+import LightNode from './LightNode.js';
+
+export default class LightElementNode extends LightNode {
+    constructor({ tagName, displayType = 'block', closingType = 'normal', classes = [] }) {
+        super();
+        this.tagName = tagName;
+        this.displayType = displayType;
+        this.closingType = closingType;
+        this.classes = classes;
+        this.children = [];
+    }
+
+    addChild(node) {
+        if (!(node instanceof LightNode)) {
+            throw new Error('Child must be LightNode');
+        }
+        this.children.push(node);
+        return this;
+    }
+
+    get childCount() {
+        return this.children.length;
+    }
+
+    get innerHTML() {
+        return this.children.map(c => c.outerHTML).join('');
+    }
+
+    get outerHTML() {
+        const cls = this.classes.length ? ` class="${this.classes.join(' ')}"` : '';
+        if (this.closingType === 'selfClosing') {
+            return `<${this.tagName}${cls}/>`;
+        }
+        return `<${this.tagName}${cls}>${this.innerHTML}</${this.tagName}>`;
+    }
+}
